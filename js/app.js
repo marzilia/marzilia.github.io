fe2a7785cc4b35af47cd8af8b475c55b0ec1a99e
zanetti marc <marzilia.dev@gmail.com>
2024-11-10 23:02:56 +0100

    version2

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 const openMenu = () => {
   const menu = document.querySelector(".header-menu");
-  menu.classList.toogle("active");
-  
-  if(menu.classList.contains ("active")){
-  
-    document.querySelector("header.material-symbols-outlined").innerHTML ="close"
-  }
-  else{
-     document.querySelector("header.material-symbols-outlined").innerHTML ="menu"
-  }
+
+  menu.classList.toggle("active");
+  if(menu.classList.contains("active"))
+     {
+       document.querySelector("header .material-symbols-outlined").innerHTML="close"
+      }
+else{
+       document.querySelector("header .material-symbols-outlined").innerHTML= "menu"
+ }
+
 }
